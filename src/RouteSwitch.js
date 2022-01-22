e8f004f2c33d327cf61636687254f368bc35ff9d
Dylan King <dwkblue51@yahoo.com>
2022-01-21 21:52:12 -0600

    continue RouteSwitch setup

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,5 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 function RouteSwitch() {
-    
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                {/* <Route path="/home" element={<App />} /> */}
+
+            </Routes>
+        </BrowserRouter>
+    )
 }
+
+export default RouteSwitch;
