9f8ebd0bdb01be50520f6a60520de715910610fe
Dylan King <dwkblue51@yahoo.com>
2022-01-25 15:23:56 -0600

    replace hardcode with .map to generate product cards

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,22 +1,13 @@
 import React from "react";
+import Card from "./Card";
 
-function Products() {
+function Products(props) {
+    const {items} = props;
     return (
         <div>
             <h1>Products</h1>
             <div>
-                <div>
-                    <h2>Product</h2>
-                </div>
-                <div>
-                    <h2>Product</h2>
-                </div>
-                <div>
-                    <h2>Product</h2>
-                </div>
-                <div>
-                    <h2>Product</h2>
-                </div>
+                {items.map(item => <Card key={item.key} image={item.image} name={item.title} />)}
             </div>
         </div>
     )
